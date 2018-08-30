05974a606ba7f92c4dfe40e12154793ecb970f94
Yash-Garg <ben10.yashgarg@gmail.com>
2018-08-30 18:25:51 +0530

    A6020: Kill IMS
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -302,7 +302,6 @@ PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.qcom.rc \
     init.qcom.sh \
-    init.qcom-ims.rc \
     init.qcom.mem.sh \
     init.qcom.post_boot.sh \
     ueventd.qcom.rc
