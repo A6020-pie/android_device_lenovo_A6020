d0553e0446bbb856aedae202f4ac4afa8d990490
Yash-Garg <ben10.yashgarg@gmail.com>
2018-09-11 22:01:56 +0000

    A6020: Cleanup and sort device tree
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>

--- board/hardware.mk
+++
@@ -1,6 +0,0 @@
-# Qualcomm support
-BOARD_USES_QCOM_HARDWARE := true
-
-# Lineage hardware
-BOARD_HARDWARE_CLASS += \
-    device/lenovo/A6020/lineagehw
--- board/power.mk
+++
@@ -1,3 +0,0 @@
-# Power
-TARGET_POWERHAL_VARIANT := qcom
-TARGET_POWERHAL_SET_INTERACTIVE_EXT := device/lenovo/A6020/power/power_ext.c
